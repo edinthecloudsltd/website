83073328d716f6e4ddc9067cc16eff853a8d9715
Ed Smith <ed@edintheclouds.io>
2022-03-26 11:43:14 +0000

    add retention for artifact and workaround fouc issue

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Footer from '../footer';
 import Navbar from '../navbar';
 
 const Layout: React.FC = ({ children }) => {
+  // Workaroud FOUC
+  const [show, setShow] = useState<boolean>(false);
+  useEffect(() => setShow(true), []);
+
   return (
-    <>
+    <div style={{ visibility: show ? 'visible' : 'hidden' }}>
       <Navbar />
       <div style={{ position: 'relative', overflow: 'hidden' }}>{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
 
