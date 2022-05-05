26286969b183b9ae48985da93247d15edb8a2ebc
Ed Smith <ed@edintheclouds.io>
2022-05-05 10:18:38 +0100

    Change workflows and update footer styles to styled-components

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 import { FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi';
-import { useMediaQuery } from 'react-responsive';
 
 import MaxWidthWrapper from 'src/components/common/max-width-wrapper';
 
@@ -9,47 +8,22 @@ import * as Styled from './styles';
 
 const Footer: React.FC = () => {
   const router = useRouter();
-  const isMobile = useMediaQuery({ query: '(max-width: 544px)' });
-
   const date = new Date().getFullYear();
 
   return (
     <Styled.Wrapper>
       <MaxWidthWrapper>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: isMobile ? 'column-reverse' : 'row',
-            justifyContent: isMobile ? '' : 'space-between',
-            gap: '1rem',
-            width: '100%',
-            height: '100%',
-            paddingBottom: '2rem',
-          }}
-        >
-          <div
-            style={{ marginTop: isMobile ? '0' : 'auto', textAlign: isMobile ? 'center' : 'left' }}
-          >
-            <p>© {date} Ed in the Clouds Consulting Limited</p>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '1rem',
-              margin: isMobile ? '1rem 0 0 auto' : 'auto 0 0 0',
-            }}
-          >
-            <div style={{ marginLeft: 'auto' }}>
-              {router.pathname === '/' && (
-                <p style={{ fontStyle: 'italic' }}>
-                  Company Number: 12110132
-                  <br />
-                  VAT Registered: 328988441
-                </p>
-              )}
-            </div>
-            <div id="social-icons" style={{ display: 'flex', gap: '1rem', marginLeft: 'auto' }}>
+        <Styled.FooterFlex>
+          <Styled.CompanyName>© {date} Ed in the Clouds Consulting Limited</Styled.CompanyName>
+          <Styled.FooterFlexRight>
+            {router.pathname === '/' && (
+              <Styled.CompanyInfo>
+                Company Number: 12110132
+                <br />
+                VAT Registered: 328988441
+              </Styled.CompanyInfo>
+            )}
+            <Styled.SocialIcons>
               <Styled.IconWrapper>
                 <a href="https://twitter.com/ed1nthecloud">
                   <FaTwitter size={25} />
@@ -65,9 +39,9 @@ const Footer: React.FC = () => {
                   <FiMail size={25} />
                 </a>
               </Styled.IconWrapper>
-            </div>
-          </div>
-        </div>
+            </Styled.SocialIcons>
+          </Styled.FooterFlexRight>
+        </Styled.FooterFlex>
       </MaxWidthWrapper>
     </Styled.Wrapper>
   );
--- src/components/common/footer/styles.tsx
+++ src/components/common/footer/styles.tsx
@@ -8,6 +8,51 @@ export const Wrapper = styled.footer`
   background: white;
 `;
 
+export const FooterFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  width: 100%;
+  height: 100%;
+  padding-bottom: 2rem;
+
+  @media (max-width: 568px) {
+    flex-direction: column-reverse;
+    justify-content: unset;
+  }
+`;
+
+export const CompanyName = styled.p`
+  margin-top: auto;
+  text-align: left;
+
+  @media (max-width: 568px) {
+    margin-top: 0;
+    text-align: center;
+  }
+`;
+
+export const FooterFlexRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: auto 0 0 0;
+
+  @media (max-width: 568px) {
+    margin: 1rem 0 0 auto;
+  }
+`;
+
+export const CompanyInfo = styled.p`
+  font-style: italic;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-left: auto;
+`;
+
 export const IconWrapper = styled.div`
   cursor: pointer;
 
