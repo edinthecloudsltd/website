cc925485f179239049ea6a284c054ce9094ee4ba
Ed Smith <ed@edintheclouds.io>
2022-11-02 18:21:47 +0000

    feat: add analytics package and remove sls-next cdk construct

--- deploy/bin/website.ts
+++ deploy/bin/website.ts
@@ -1,14 +1,8 @@
 import * as path from 'path';
 import { Construct } from 'constructs';
-import { App, Fn, StackProps } from 'aws-cdk-lib';
-import { Builder } from '@sls-next/lambda-at-edge';
-
-import { ServerlessNextJsLambdaEdge } from '../lib/sls-next-lambda-edge';
+import { App, StackProps } from 'aws-cdk-lib';
 import { CognitoUserPool } from '../lib/cognito';
 import { WebsiteDynamoDBTables } from '../lib/dynamodb';
-
-const nextConfigPath = path.resolve(`${process.cwd()}/../`);
-const outputDir = path.resolve(`${nextConfigPath}/.serverless_nextjs`);
 
 export interface IWebsiteProps extends StackProps {
   environment: string;
@@ -41,6 +35,7 @@ const env: IWebsiteConfigMap = {
       'arn:aws:acm:us-east-1:912474597003:certificate/e21d9428-5b12-4fc9-96eb-a6b155f02ff3',
   },
 };
+
 export class Deployment extends Construct {
   constructor(scope: Construct, id: string, props: IWebsiteProps) {
     super(scope, id);
@@ -49,25 +44,8 @@ export class Deployment extends Construct {
       new CognitoUserPool(this, `WebsiteCognito`, props);
     }
     new WebsiteDynamoDBTables(this, 'WebsiteDynamoDBTables', props);
-    new ServerlessNextJsLambdaEdge(this, 'Website', props);
   }
 }
 
 const app = new App();
-
-const builder = new Builder(nextConfigPath, outputDir, {
-  cwd: nextConfigPath, // root of our project
-  cmd: 'node_modules/.bin/next',
-  args: ['build'],
-  minifyHandlers: true,
-});
-
-builder
-  .build()
-  .then(() => {
-    new Deployment(app, 'EdintheClouds', env[process.env.ENVIRONMENT || 'development']);
-  })
-  .catch((e: any) => {
-    console.log('Could not build app due the exception: ', e);
-    process.exit(1);
-  });
+new Deployment(app, 'EdintheClouds', env[process.env.ENVIRONMENT || 'development']);
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
+import { Analytics } from '@vercel/analytics/react';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
 import Auth from 'src/components/auth';
 import Layout from 'src/components/layout/layout';
 import { DisplayProvider } from 'src/context/display';
-// import Script from 'next/script';
 
 // Used to import tailwindcss
 import '../styles/global.css';
-
-// const gtagId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export default function App({ Component, pageProps }: AppProps) {
   if (process.env.VERCEL_ENV === 'preview') {
@@ -29,10 +27,13 @@ export default function App({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <DisplayProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </DisplayProvider>
+    <>
+      <DisplayProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DisplayProvider>
+      <Analytics />
+    </>
   );
 }
